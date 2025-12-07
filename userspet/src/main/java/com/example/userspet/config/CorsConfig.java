7a1fa10cfa06c54ef4ba58bce502c1ae36e3e19d
Yangshan-Liu55 <yangshan.liu55@gmail.com>
2025-12-07 22:18:59 +0100

    Allow all

--- userspet/src/main/java/com/example/userspet/config/CorsConfig.java
+++ userspet/src/main/java/com/example/userspet/config/CorsConfig.java
@@ -14,11 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(
-                                "http://localhost:3000",
-                                "http://localhost:5173",
-                                "https://users-with-pets.onrender.com/"
-                        )
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
